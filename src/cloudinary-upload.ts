1f49f024bdffc0ce13be17ac1a1da4924fbd3e4c
walidChaybi <98161047+walidChaybi@users.noreply.github.com>
2023-12-24 12:41:11 +0100

    added helper functions

---
+++ src/cloudinary-upload.ts
@@ -0,0 +1,57 @@
+import cloudinary, {
+  UploadApiErrorResponse,
+  UploadApiResponse,
+} from 'cloudinary';
+
+export function upload(
+  file: string,
+  public_id: string,
+  overwrite?: boolean,
+  invalidate?: boolean
+): Promise<UploadApiResponse | UploadApiErrorResponse | undefined> {
+  return new Promise((resolve) => {
+    cloudinary.v2.uploader.upload(
+      file,
+      {
+        public_id,
+        overwrite,
+        invalidate,
+        resource_type: 'auto',
+      },
+      (
+        err: UploadApiErrorResponse | undefined,
+        res: UploadApiResponse | undefined
+      ) => {
+        if (err) return resolve(err);
+        resolve(res);
+      }
+    );
+  });
+}
+
+export function videoUpload(
+  file: string,
+  public_id: string,
+  overwrite?: boolean,
+  invalidate?: boolean
+): Promise<UploadApiResponse | UploadApiErrorResponse | undefined> {
+  return new Promise((resolve) => {
+    cloudinary.v2.uploader.upload(
+      file,
+      {
+        public_id,
+        overwrite,
+        invalidate,
+        chunk_size: 50000,
+        resource_type: 'video',
+      },
+      (
+        err: UploadApiErrorResponse | undefined,
+        res: UploadApiResponse | undefined
+      ) => {
+        if (err) return resolve(err);
+        resolve(res);
+      }
+    );
+  });
+}
